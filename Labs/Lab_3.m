50029afb0d00bc3b884d13c943cc914fee4f6ce8
Josh Carlile <josh.carlile@Joshs-MacBook-Air.local>
2021-01-28 09:26:11 -0800

    Removed unfinished error handling

--- Labs/Lab_3.m
+++ Labs/Lab_3.m
@@ -7,6 +7,10 @@ clc
 % Date: 1/31/2021
 %
 
+function Lab_3 ()
+    fprintf("This ran automatically");
+end
+
 %% QUESTION 1
 
 length = 15;
@@ -22,16 +26,26 @@ array = ["Oxygen"   15.9994     2;
 fprintf("The weight of hydrogen peroxide is %.2f amu\n", amu(array));
 
 %% QUESTION 3
+% Write a function that will calculate the volume of a hollow sphere
 
 % See volume(r1, r2)
 
 %% QUESTION 4
+% Wing loading is the airplane's weight divided by wing area. Write a
+% function that will prompt the user for the weight in kg, and wing area in
+% m^2. Return wing loading in kg / m^2
 
-% See wingLoading (weight, wingArea)
+% See wingLoading ()
 
 %% QUESTION 5
 
+
+
+%% QUESTION 6
+
 foodCost(100000);
+
+%% QUESTION 7
 
 %% FUNCTION DEFINITIONS
 
@@ -54,8 +68,13 @@ function volume = volHollowSphere (r0, r1)
     volume = (4*pi / 3) * (r1^3 - r0^3);
 end
 
-function r = wingLoading (weight, wingArea)
+function r = wingLoading ()
     % Weight in kg, wingArea in m^2, r in kg / m^2
+    
+    % Prompt user for airplane weight in kg
+    weight = input("Enter the airplane weight in kg: ");
+    
+    
     r = weight / wingArea;
     % Matlab stores values to full machine precision, meaning x.yzq000000000000000000...
     % Any display of simply x.yzq is just a display preference, and not the
