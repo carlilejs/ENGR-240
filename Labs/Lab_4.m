3c7fc04ef54eb3caa4d4e9d46be916dacb9ab2d8
Josh Carlile <carlilejs@gmail.com>
2021-02-04 09:37:15 -0800

    Finished question 1

---
+++ Labs/Lab_4.m
@@ -0,0 +1,37 @@
+clear
+clc
+
+%
+% Author: Josh Carlile
+% Class: Mr. Hogan's ENGR 240
+% Date: 2/4/2020
+%
+
+%% QUESTION 1
+% Create a script that creates variables X and Y. Assign values to these
+% variables and plot them using green +.
+
+X = linspace (1, 2*pi);
+Y = 1 - sin (X).^2;
+plot(X, Y, 'g+');
+
+%% QUESTION 2
+
+
+%% QUESTION 3
+
+
+
+%% QUESTION 4
+
+
+
+%% QUESTION 5
+
+
+
+%% QUESTION 6
+
+
+
+%% FUNCTION DEFINITIONS
