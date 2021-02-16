34bcb5d065a873be6140ac3b390a58bec7df9bfc
Josh Carlile <carlilejs@gmail.com>
2021-02-16 11:03:12 -0800

    Easy problems

--- Labs/Lab_5.m
+++ Labs/Lab_5.m
@@ -7,3 +7,21 @@ clc
 
 %% Question 1
 
+syms x y z
+
+%% Question 2
+
+expr = x^2 + 3*x + 6 % Expression will print to console
+
+%% Question 3
+
+S = simplify (expr) % Already in simplest form
+
+%% Question 4
+
+F = factor (expr) % Does not factor
+
+%% Question 5
+
+expr2 = y^2 + 4*y^3 +3*y^2 + 3y + y
+
